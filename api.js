d940fc34d63118aa2389043d71759cd08437d9f2
nanangqq <gingggg@gmail.com>
2021-05-14 22:50:49 +0900

    db test

--- api.js
+++ api.js
@@ -4,8 +4,12 @@ import Axios from 'axios'
 
 const api = new Router()
 
-api.get('/test', ctx => {
-    ctx.body = 'test ok'
+api.get('/test', async ctx => {
+    const res = await executeQ(
+        'select st_asgeojson(st_transform(geometry, 5186)) from seoul_jijuk_0501_4326 limit 1'
+    )
+    // console.log(res.rows)
+    ctx.body = res.rows
 })
 
 export default api
